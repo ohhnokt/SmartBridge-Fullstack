111f0aea047e1cfe1e8810174f60ef921f8982b1
Kartavya Chaudhary <kartavya08.02@gmail.com>
2025-04-15 01:31:22 +0530

    CustomButton.jsx

---
+++ Assignment3/src/components/CustomButton.jsx
@@ -0,0 +1,17 @@
+function CustomButton() {
+    const handleClick = () => {
+      console.log("Button clicked!");
+    };
+  
+    return (
+      <button
+        onClick={handleClick}
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+      >
+        Click Me
+      </button>
+    );
+  }
+  
+  export default CustomButton;
+  
